30d16d5a6e9db55df13d123618ab255cdffb372c
Giorgio Garasto <giorgio@garasto.me>
2022-10-07 17:54:51 -0300

    feat: add index file

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as certifications } from './certifications';
+export { default as projects } from './projects';
+export { default as skills } from './skills';
+export { default as socials } from './socials';
+export { default as timeline } from './timeline';
